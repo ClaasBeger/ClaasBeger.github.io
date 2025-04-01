d9e2b1d18a42c8cbd19deb7a594efa7056cf2bbb
ClaasBeger <51171693+ClaasBeger@users.noreply.github.com>
2025-04-01 22:32:01 +0000

    Deploying to gh-pages from @ ClaasBeger/ClaasBeger.github.io@385bec4629dd4c19b6ab7445bedbb643cf52f870 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "A growing collection of my projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
