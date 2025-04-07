0b47059d753e0aff851cd64fa9f7379bb752a44e
ClaasBeger <51171693+ClaasBeger@users.noreply.github.com>
2025-04-07 22:48:40 +0000

    Deploying to gh-pages from @ ClaasBeger/ClaasBeger.github.io@e1e88bf790edd072800a209da8d93335b855b7a7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "An overview of my publicly hosted repositories.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
